a9de1cbcd3a3aa75e84f887445e3146242c42fb3
dzekem <cdzekem@gmail.com>
2022-10-24 17:27:23 +0100

    Added user model

---
+++ models.py
@@ -0,0 +1,8 @@
+from . import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(200))
+    name = db.Column(db.String(200))
